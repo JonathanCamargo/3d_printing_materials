672bc14dc82e4af6ce886da91fe4570d3022142c
JPZapa <93365790+JPZapa@users.noreply.github.com>
2024-12-02 10:28:45 -0500

    Add files via upload

--- pages/properties.py
+++ pages/properties.py
@@ -1,1 +1,10 @@
-# Visualize and compare material properties
+import streamlit as st
+from base2 import sel_propiedades
+
+options=st.multiselect("Seleccione los casos a considerar", st.session_state.conjunto.keys())
+
+dfE,dfF=sel_propiedades(st.session_state.propiedades,options)
+
+dfE
+
+dfF
--- pages/raw.py
+++ pages/raw.py
@@ -1,1 +1,10 @@
-# Visualize raw testing data
+import streamlit as st
+from base2 import graficar_conjunto
+
+
+
+options=st.multiselect("Seleccione los casos a graficar", st.session_state.conjunto.keys())
+
+fig=graficar_conjunto(st.session_state.conjunto,options)
+
+st.pyplot(fig)
