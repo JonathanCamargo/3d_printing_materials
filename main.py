6bde127276fed5ab30c4aa0cdcaa4e4a85f9495b
JPZapa <93365790+JPZapa@users.noreply.github.com>
2024-12-02 10:29:48 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-# streamlit main.py 
+import streamlit as st
+import sys
 
-# Loads pages. Every GUI app is inside the pages folder
+sys.path.insert(0, r'C:/Users/juanp/Downloads/Caracterizacion/Parser')
+
+from base2 import cargar_conjunto,propiedades
+
+
+st.session_state.conjunto=cargar_conjunto()
+st.session_state.propiedades=propiedades(st.session_state.conjunto)
